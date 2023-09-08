19c7ce4d46800f67fc1857169bbeaa6fdd4f6752
João Vitor Espig <jotaespig@gmail.com>
2023-09-08 14:17:46 -0300

    test(main.py): testing launch_into_orbit

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from mechpy import mechpy
 def main() -> None:
     mech = mechpy.MechPy()
     print(mech.conn.krpc.get_status().version)
-    mech.do_maneuver()
+    #mech.do_maneuver()
+    mech.launch_into_orbit(80000, 3000, 35000)
 
 
 if __name__ == "__main__":
