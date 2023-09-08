161310f20c0be1f7b6520c3ebf83b80f451e402f
João Vitor Espig <jotaespig@gmail.com>
2023-09-08 16:13:17 -0300

    test(main.py): testing suicide burn

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ def main() -> None:
     mech = mechpy.MechPy()
     print(mech.conn.krpc.get_status().version)
     #mech.do_maneuver()
-    mech.launch_into_orbit(80000, 3000, 35000)
+    #mech.launch_into_orbit(80000, 3000, 35000)
+    mech.suicide_burn()
 
 
 if __name__ == "__main__":
