16e271184b70d1cfc75dd98c35a723bd203dc91b
João Vitor Espig <jotaespig@gmail.com>
2023-09-09 13:18:50 -0300

    test: testing

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ from mechpy import mechpy
 def main() -> None:
     mech = mechpy.MechPy()
     print(mech.conn.krpc.get_status().version)
+    print(mech.conn.space_center.active_vessel.auto_pilot.reference_frame)
     #mech.do_maneuver()
-    mech.launch_into_orbit(80000, 1000, 40000)
+    mech.launch_into_orbit(20000, 4000, 8000)
     #mech.suicide_burn()
 
 
