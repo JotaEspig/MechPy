40e2a0a00dd79de07163768462725e72af3d78bf
João Vitor Espig <jotaespig@gmail.com>
2023-09-08 23:27:09 -0300

    feat(mechpy): show messages ingame when doing something

--- mechpy/mechpy.py
+++ mechpy/mechpy.py
@@ -10,12 +10,18 @@ from mechpy import suicideburn
 class MechPy:
     def __init__(self):
         self.conn: krpc.Client = krpc.connect(name="MechPy")
+        self.conn.ui.message("MechPy\nVersion: v0.1", duration=5)
     
     def __del__(self):
         self.conn.close()
 
 
+    def show_message(self, content: str, duration=1) -> None:
+        self.conn.ui.message(content, duration=duration)
+
     def do_maneuver(self):
+        self.show_message("Doing maneuver", duration=2)
+
         vessel = self.conn.space_center.active_vessel
         if len(vessel.control.nodes) == 0:
             return
@@ -25,11 +31,15 @@ class MechPy:
 
     def launch_into_orbit(self, target_alt: int, turn_start_alt: int,
                           turn_end_alt: int) -> None:
+        self.show_message("Launching into orbit", duration=2)
+
         vessel = self.conn.space_center.active_vessel
         launch.Launch(
             self.conn, vessel, target_alt, turn_start_alt, turn_end_alt
         ).do()
 
     def suicide_burn(self) -> None:
+        self.show_message("Doing SUICIDE BURN\nI hope we don't crash", duration=2)
+
         vessel = self.conn.space_center.active_vessel
         suicideburn.SuicideBurn(self.conn, vessel).do()
