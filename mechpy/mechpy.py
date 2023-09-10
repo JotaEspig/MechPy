191fe38e61403a353c9705dd970934c6e4e84280
João Vitor Espig <jotaespig@gmail.com>
2023-09-10 19:22:19 -0300

    refactor: minor changes

--- mechpy/mechpy.py
+++ mechpy/mechpy.py
@@ -1,18 +1,23 @@
 import krpc
 
+from mechpy import displayer
 from mechpy import launch
 from mechpy import maneuver
 from mechpy import suicideburn
+from mechpy.__version__ import __version__
+
 
 class MechPy:
     def __init__(self):
         self.conn: krpc.Client = krpc.connect(name="MechPy")
-        self.conn.ui.message("MechPy\nVersion: v0.1", duration=5)
-    
-    def __del__(self):
-        self.conn.ui.message("MechPy closed", duration=5)
-        self.conn.close()
+        self.conn.ui.message(f"MechPy\nVersion: v{__version__}", duration=5)
+        self.display = displayer.Displayer(self.conn)
 
+    def __del__(self):
+        if self.conn != None:
+            del self.display
+            self.conn.ui.message("MechPy closed", duration=5)
+            self.conn.close()
 
     def do_maneuver(self):
         self.conn.ui.message("Doing maneuver", duration=2)
@@ -34,7 +39,8 @@ class MechPy:
         ).do()
 
     def suicide_burn(self) -> None:
-        self.conn.ui.message("Doing SUICIDE BURN\nI hope we don't crash", duration=2)
+        self.conn.ui.message(
+            "Doing SUICIDE BURN\nI hope we don't crash", duration=2)
 
         vessel = self.conn.space_center.active_vessel
         suicideburn.SuicideBurn(self.conn, vessel).do()
