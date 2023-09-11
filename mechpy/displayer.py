9d12a1f5d7b5fad0d221fa967328cf84eab411f2
João Vitor Espig <jotaespig@gmail.com>
2023-09-10 21:52:02 -0300

    feat: added support for texts, input fields and buttons

--- mechpy/displayer.py
+++ mechpy/displayer.py
@@ -1,29 +1,31 @@
 from typing import Tuple, List
 
 import krpc
+from krpc import stream
 from krpc.services import ui
 
 
 class Displayer:
-    RECT_SIZE = (300, 200)
-
-    def __init__(self, conn: krpc.Client) -> None:
+    def __init__(self, conn: krpc.Client, size: Tuple[float, float],
+                 pos: Tuple[float, float]) -> None:
         canvas = conn.ui.stock_canvas
-        screen_size = canvas.rect_transform.size
+        panel = canvas.add_panel()
+        rect = panel.rect_transform
+        rect.size = size
+        rect.position = pos
 
         self.conn = conn
-        self.panel = canvas.add_panel()
-        self.rect = self.panel.rect_transform
-        self.rect.size = self.RECT_SIZE
-        self.rect.position = (110 - (screen_size[0]/2), 0)
+        self.panel = panel
+        self.rect = rect
         self.texts: List[ui.Text] = []
+        self.buttons: List[ui.Button] = []
+        self.input_fields: List[ui.InputField] = []
 
     def __del__(self) -> None:
         if self.conn != None:
-            self.conn.ui.clear()
+            self.panel.remove()
 
-    def add_text(self, content: str,
-                 pos: Tuple[float, float] = (0, 0),
+    def add_text(self, content: str, pos: Tuple[float, float] = (0, 0),
                  color: Tuple[float, float, float] = (0, 0, 0),
                  font_size: int = 18) -> int:
         text = self.panel.add_text(content)
@@ -35,7 +37,29 @@ class Displayer:
         return len(self.texts) - 1
 
     def update_text_content(self, idx: int, content: str) -> None:
-        if len(self.texts) <= idx:
-            raise Exception("Invalid index to Displayer.texts")
-
         self.texts[idx].content = content
+
+    def add_button(self, content: str,
+                   pos: Tuple[float, float] = (0, 0)) -> int:
+        btn = self.panel.add_button(content)
+        btn.rect_transform.position = pos
+        self.buttons.append(btn)
+        return len(self.buttons) - 1
+
+    def get_button_clicked_stream(self, idx: int) -> stream.Stream:
+        btn_stream = self.conn.add_stream(
+            getattr, self.buttons[idx], "clicked"
+        )
+        return btn_stream
+
+    def add_input_field(self, pos: Tuple[float, float] = (0, 0)) -> int:
+        input_field = self.panel.add_input_field()
+        input_field.rect_transform.position = pos
+        self.input_fields.append(input_field)
+        return len(self.input_fields) - 1
+
+    def get_input_field_changed_stream(self, idx: int) -> stream.Stream:
+        input_stream = self.conn.add_stream(
+            getattr, self.input_fields[idx], "changed"
+        )
+        return input_stream
--- mechpy/mechpy.py
+++ mechpy/mechpy.py
@@ -9,14 +9,20 @@ from mechpy.__version__ import __version__
 
 class MechPy:
     def __init__(self):
-        self.conn: krpc.Client = krpc.connect(name="MechPy")
+        conn = krpc.connect(name="MechPy")
+        screen_size = conn.ui.stock_canvas.rect_transform.size
+
+        self.conn = conn
         self.conn.ui.message(f"MechPy\nVersion: v{__version__}", duration=5)
-        self.display = displayer.Displayer(self.conn)
+        self.display = displayer.Displayer(
+            self.conn, (300, 200), (151 - screen_size[0]/2, 200)
+        )
 
     def __del__(self):
         if self.conn != None:
             del self.display
             self.conn.ui.message("MechPy closed", duration=5)
+            self.conn.ui.clear()
             self.conn.close()
 
     def do_maneuver(self):
